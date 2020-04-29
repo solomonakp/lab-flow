6dcb34738d467cd02c9499cc128251991ba99529
Chokor Solomon <solomonakp@gmail.com>
2020-04-29 15:39:44 +0100

    nav-bar done yet responseive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+
+import Header from './components/layouts/Header';
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>hi</header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/layouts/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../component styles/header.css';
+function Header() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light py-3 fixed-top'>
+      <a className='navbar-brand d-none d-lg-block' href='./'>
+        LAB FLOW
+      </a>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarSupportedContent'
+        aria-controls='navbarSupportedContent'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon' />
+      </button>
+
+      <form className='form-inline search-form  ml-auto mr-4 '>
+        <div className='input-group align-items-center '>
+          <i className='material-icons mr-2'>search</i>
+          <input
+            type='search'
+            placeholder='Search...'
+            aria-label='Search'
+            aria-describedby='search-patient'
+          />
+        </div>
+      </form>
+
+      <ul className='navbar-nav  flex-row'>
+        <li className='nav-item active mr-3  '>
+          <a className='nav-link text-success' href='/'>
+            Sunny Asar
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a className='nav-link text-danger' href='/'>
+            Log out
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
